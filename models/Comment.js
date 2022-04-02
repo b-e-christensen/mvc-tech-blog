83b8276b719d3d611a96c91c0191abc742778661
Ben Christensen <ben.eric.christensen@gmail.com>
2022-04-01 20:04:17 -0600

    added some more views/basic model structure

---
+++ models/Comment.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        // need relation id's here (posts and users.)
+    },    
+        {
+            sequelize,
+            timestamps: true,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'comment',
+        }
+)
+
+module.exports = Comment
--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 const User = require('./User');
 
 module.exports = { User };
+
+// User has many Posts
+
+// Posts belong to User
+
+// Posts has many Comments
+
+// Comments belong to Posts
