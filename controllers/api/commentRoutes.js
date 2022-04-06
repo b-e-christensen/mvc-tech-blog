08c15cbb72948516bfdd063b535f7aa755727116
Ben Christensen <ben.eric.christensen@gmail.com>
2022-04-05 18:37:54 -0600

    heroku commit

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -61,7 +61,7 @@ router.get('/delete/:id', async (req, res) => {
     }
   })
 
-  router.post('/', withAuth, async (req, res) => {
+  router.post('/', async (req, res) => {
     try {
         const newComment = await Comment.create({
             content: req.body.content,
