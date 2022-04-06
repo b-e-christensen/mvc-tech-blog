147742cb8e1b555b04837a6ae34fa0ccf4e7154d
Ben Christensen <ben.eric.christensen@gmail.com>
2022-04-05 18:13:32 -0600

    changed variable name

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SESS_SECRET,
+  secret: process.env.SECRET_SESSION,
   cookie: {},
   resave: false,
   saveUninitialized: true,
